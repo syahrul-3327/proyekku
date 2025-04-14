d0550ac7f76365ecd880918533828dfb5bb2b75f
syahrul-3327 <82913942+syahrul-3327@users.noreply.github.com>
2025-04-14 21:23:18 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('Aplikasi Silsilah Keluarga Bani Achmad Zabidi siap digunakan!');
+    
+    // Add interactive functionalities here
+});
